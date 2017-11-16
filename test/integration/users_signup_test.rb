d56640c6c07f28133b51108f83bd7bb3883fee23
Ajmal Nabil <ajmalnabiljamal@gmail.com>
2017-11-16 15:18:19 +0800

    Use SSL and the Puma webserver in production

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -14,15 +14,14 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   end
 
   test "valid signup information" do
-    get signup_path
-    assert_difference 'User.count', 1 do
-      post users_path, params: { user: { name:  "",
-                                         email: "user@invalid",
-                                         password: "foo",
-                                         password_confirmation: "bar" }}
-
-    end
-    follow_redirect!
-    assert_template 'users/show'
-  end
+     get signup_path
+     assert_difference 'User.count', 1 do
+       post users_path, params: { user: { name:  "Example User",
+                                          email: "user@example.com",
+                                          password:              "password",
+                                          password_confirmation: "password" } }
+     end
+     follow_redirect!
+     assert_template 'users/show'
+   end
 end
